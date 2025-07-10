eab9759483df045247b56ae510252d6fa05efb84
1kantana <126508708+1kantana@users.noreply.github.com>
2025-07-10 13:24:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 import pandas as pd
 from io import BytesIO
 
-st.title("บันทึกรายจ่าย Happy 💰😊")
+st.title("Expense Tracker TH")
 
 data = st.text_area("Enter your data (format: '7 Jul: rice-meal 60 coffee-meal 50')", height=200)
 
