bfa073bf5f64b035d9f7316e6688bf7f5ddff3ef
1kantana <126508708+1kantana@users.noreply.github.com>
2025-08-07 10:05:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,9 +16,9 @@ def is_weekend(date_str):
     return dt.weekday() >= 5
 
 CATEGORY_EMOJI = {
-    "food": "",
+    "food": "🥩",
     "drink": "🥤",
-    "shopping": "",
+    "shopping": "💰",
     "misc": "📦",
     "health": "💊",
 }
@@ -87,6 +87,7 @@ if st.button("Calculate"):
         file_name="expenses_report.xlsx",
         mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
     )
+
 
 
 
